a6edb35cb7ddf60d57c2dd7fdfe1b791211e5a87
Bilal <bilal33@hotmail.com>
2011-11-01 23:44:49 -0400

    finished part 4 of HW3

--- ex3/mlclass-ex3/predict.m
+++ ex3/mlclass-ex3/predict.m
@@ -9,7 +9,7 @@ num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
-
+%dbstop('predict',1);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -21,9 +21,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
+X = [ones(m,1) X];
+a2 = sigmoid(Theta1*X');
+a2 = [ones(m,1) a2'];
+out = sigmoid(Theta2*a2');
+[values,p]= max(out',[],2);
 
 
 
